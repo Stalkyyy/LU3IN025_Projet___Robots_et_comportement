5d13e03e779968d15d9fe529f2575c2cb4a7be69
guest <guest@Ubuntu2024.myguest.virtualbox.org>
2024-01-30 21:05:14 +0100

    Amélioration TP1 + Projet

--- paintwars_team_challenger.py
+++ paintwars_team_challenger.py
@@ -1,23 +1,112 @@
 # Projet "robotique" IA&Jeux 2021
 #
 # Binome:
-#  Prénom Nom: _________
-#  Prénom Nom: _________
+#  Prénom Nom: Enzo PINHO FERNANDES
+#  Prénom Nom: Maximilien Piron-Palliser
+
+import random
+import math
 
 def get_team_name():
-    return "[ team with no name ]" # à compléter (comme vous voulez)
-
+    return "SILKSONG PITY" # à compléter (comme vous voulez)
+    
+def get_extended_sensors(sensors):
+    for key in sensors:
+        sensors[key]["distance_to_robot"] = 1.0
+        sensors[key]["distance_to_wall"] = 1.0
+        if sensors[key]["isRobot"] == True:
+            sensors[key]["distance_to_robot"] = sensors[key]["distance"]
+        else:
+            sensors[key]["distance_to_wall"] = sensors[key]["distance"]
+    return sensors
+    
+    
 def step(robotId, sensors):
+    sensors = get_extended_sensors(sensors)
 
-    translation = 1 # vitesse de translation (entre -1 et +1)
-    rotation = 0 # vitesse de rotation (entre -1 et +1)
 
-    if sensors["sensor_front_left"]["distance"] < 1 or sensors["sensor_front"]["distance"] < 1:
-        rotation = 0.5  # rotation vers la droite
-    elif sensors["sensor_front_right"]["distance"] < 1:
-        rotation = -0.5  # rotation vers la gauche
+    if testDetectionRobotFront(sensors) :
+        # Si un robot adverse est devant nous, et qu'on est un robot assigné, on le suit. Sinon on l'évite.
+        if not isSameTeamFront(sensors) : 
+            if robotId%5 == 0 :
+                translation, rotation = aller_vers_les_robots(sensors) 
+            else :
+                translation, rotation = eviter_les_robots_adverse(sensors)
 
-    if sensors["sensor_front"]["isRobot"] == True and sensors["sensor_front"]["isSameTeam"] == False:
-        enemy_detected_by_front_sensor = True # exemple de détection d'un robot de l'équipe adversaire (ne sert à rien)
+        # Si un robot allié est devant nous, on tourne vers la droite.
+        else :
+            translation, rotation = eviter_les_robots_ally(sensors)
 
+    # Si on détecte un mur, on l'évite.
+    elif testDetectionWallFront(sensors) :
+        translation, rotation = eviter_les_murs(sensors)
+
+    # Si un robot adverse nous suit derrière, on s'arrête.
+    elif testDetectionStalker(sensors) and not isSameTeamBack(sensors) :
+        translation, rotation = stopStalker(sensors)
+
+    # On continue au hasard.
+    else :
+        translation, rotation = avancer_no_opti(robotId, sensors)
+        
+
+    # limite les valeurs de sortie entre -1 et +1
+    translation = max(-1,min(translation,1))
+    rotation = max(-1, min(rotation, 1))
+    
     return translation, rotation
+    
+# ==============================================================================================================
+    
+def testDetectionWallFront(sensors) :
+    return sensors["sensor_front"]["distance_to_wall"] * sensors["sensor_front_left"]["distance_to_wall"] * sensors["sensor_front_right"]["distance_to_wall"] < 0.5
+    
+def testDetectionRobotFront(sensors) :
+    return sensors["sensor_front"]["distance_to_robot"] * sensors["sensor_front_left"]["distance_to_robot"] * sensors["sensor_front_right"]["distance_to_robot"] < 1
+
+def testDetectionStalker(sensors) :
+    return sensors["sensor_back"]["distance_to_robot"] * sensors["sensor_back_left"]["distance_to_robot"] * sensors["sensor_back_right"]["distance_to_robot"] < 0.8
+
+# ==============================================================================================================
+
+def isSameTeamFront(sensors) :
+    return sensors["sensor_front"]["isSameTeam"] or sensors["sensor_front_left"]["isSameTeam"] or sensors["sensor_front_right"]["isSameTeam"]
+
+def isSameTeamBack(sensors) :
+    return sensors["sensor_back"]["isSameTeam"] or sensors["sensor_back_left"]["isSameTeam"] or sensors["sensor_back_right"]["isSameTeam"]
+
+# ==============================================================================================================
+
+# Braitenberg
+
+def avancer_no_opti(sensors) :
+    translation = 1
+    rotation = random.uniform(-0.20, 0.20)
+    return translation, rotation
+
+def eviter_les_murs(sensors) :
+    translation = 1 * sensors["sensor_front"]["distance_to_wall"]
+    rotation = (3) * sensors["sensor_front_right"]["distance_to_wall"] + (-2) * sensors["sensor_front_left"]["distance_to_wall"] + (-1) * sensors["sensor_front"]["distance_to_wall"]
+    return translation, rotation
+    
+def aller_vers_les_robots(sensors) :
+    translation = 1
+    rotation = (-1) * sensors["sensor_front_right"]["distance_to_robot"] + (1) * sensors["sensor_front_left"]["distance_to_robot"]
+    return translation, rotation
+
+def eviter_les_robots_ally(sensors) :
+    translation = 1 * sensors["sensor_front"]["distance_to_robot"]
+    rotation = 1
+    return translation, rotation
+
+def eviter_les_robots_adverse(sensors) :
+    translation = 1 * sensors["sensor_front"]["distance_to_robot"]
+    rotation = (3) * sensors["sensor_front_right"]["distance_to_robot"] + (-2) * sensors["sensor_front_left"]["distance_to_robot"] + (-1) * sensors["sensor_front"]["distance_to_wall"]
+    return translation, rotation
+
+def stopStalker(sensors) :
+    translation = (0.15) * sensors["sensor_back"]["distance_to_robot"] + (0.15) * sensors["sensor_back_left"]["distance_to_robot"] + (0.15) * sensors["sensor_back_right"]["distance_to_robot"]
+    rotation = 0
+    return translation, rotation
+
+# ==============================================================================================================
